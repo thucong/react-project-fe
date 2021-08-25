5d0ad11fa58349bf01b4fd22072d88b3358d44ca
thucong <51808157+thucong@users.noreply.github.com>
2021-08-25 13:13:20 +0700

    test

--- pages/index.js
+++ pages/index.js
@@ -12,21 +12,10 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1>Test deploy</h1>
+        <h1>Test deploy11111111111111</h1>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+    
     </div>
   )
 }
